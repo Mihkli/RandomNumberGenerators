4326a2d8dac9118935607e8a7ecf241d3e0d499a
Opilane TTHK <opilane@tthk.ee>
2021-09-08 12:17:54 +0300

    first commit

---
+++ DiceGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            string PlayerOne = "Sina";
+            string PlayerTwo = "Arvuti";
+
+            int MyRandomNumber = rnd.Next(1, 7);
+            Console.WriteLine($"{PlayerOne}, veeretasid {MyRandomNumber}.");
+            int Myrandom = rnd.Next(1, 7);
+            Console.WriteLine($"{PlayerTwo}, veeretas {Myrandom}.");
+            if(MyRandomNumber > Myrandom)
+            {
+                Console.WriteLine($"{PlayerOne} Võitsid!");
+            }
+            else if(MyRandomNumber < Myrandom)
+            {
+                Console.WriteLine($"{PlayerTwo} Võitis!");
+            }
+            else
+            {
+                Console.WriteLine("Viik, Proovige uuesti");
+            }
+            
+
+
+
+
+
+
+        }
+    }
+}
---
+++ RandomNumerGenerators/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RandomNumerGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int MyRandomNumber = rnd.Next(1, 7);
+
+            //programm genereerib juhuslikku numbrit 1-10
+            //programm kontrollib, kas genereeritud number on suurem kui 5
+            // kui see number on suurem kui 5 siis konsool kuvab "juhuslik number on {MyRandomNumber},
+            // suurem on suurem, kui 5";
+
+
+            Console.WriteLine(MyRandomNumber);
+            if(MyRandomNumber > 5)
+            {
+                Console.WriteLine($"Juhuslik number on {MyRandomNumber}, see on suurem, kui 5");
+            }
+            else if(MyRandomNumber < 5)
+            {
+                Console.WriteLine($"Juhuslik number on {MyRandomNumber}, see on väiksem, kui 5");
+            }
+            else
+            {
+                Console.WriteLine($"Juhuslik number on {MyRandomNumber}");
+            }       
+
+
+
+
+        }
+    }
+}
